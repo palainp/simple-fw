b9f2f497305c9904ea56dd478664796192285963
anhndd <anhndd1510@gmail.com>
2024-10-10 15:50:22 +0200

    update parameter config.ml

--- config.ml
+++ config.ml
@@ -1,4 +1,4 @@
-(* mirage >= 4.6.0 & < 4.8.0 *)
+(* mirage >= 4.7.0 & < 4.8.0 *)
 open Mirage
 
 (* we need two network interfaces: a public side and a private side *)
@@ -19,32 +19,20 @@ let private_netif =
 let public_ethernet = ethif public_netif
 let private_ethernet = ethif private_netif
 
-(* use the functional address resolution protocol *)
-let public_arpv4 = arp public_ethernet
-let private_arpv4 = arp private_ethernet
-
-(* finally, use statically configured (at build or runtime) ipv4 addresses.
-   (you might want to use dhcp to configure the address on the public interface.
-     this is possible, but the code is a bit too convoluted for a good example.
-     for now, we'll use statically configured addresses on both interfaces. *)
-
-let public_ipv4 = create_ipv4 ~group:"public" public_ethernet public_arpv4
-let private_ipv4 = create_ipv4 ~group:"private" private_ethernet private_arpv4
-
 let packages = [
   package "ethernet";
-  package ~min:"7.0.0" "tcpip";
-  package ~min:"4.0.0" "mirage-runtime";
+  package "ipaddr";
+  package ~min:"8.2.0" ~sublibs:["ipv4";"tcp"] "tcpip"; (* There was a breaking API changes in the 8.2 series *)
+  package "logs";
+  (* package ~min:"4.0.0" "mirage-runtime"; *) (* We want to avoid runtime argument at any cost *)
 ]
 
-(* our unikernel needs to know about physical network, ethernet, arp, and ipv4
-   modules for each interface. Even though these modules will be the same for
-   both interfaces in our case, we have to pass them separately. *)
+(* our unikernel needs to know about physical network, and ethernet modules
+   for each interface. Even though these modules will be the same for both
+   interfaces in our case, we have to pass them separately. *)
 let main = main "Unikernel.Main" ~packages
            (network  @-> network  @->
             ethernet @-> ethernet @->
-            arpv4    @-> arpv4    @->
-            ipv4     @-> ipv4     @->
             random   @-> mclock   @-> job)
 
 (* we need to pass each of the network-related impls we've made to the
@@ -52,7 +40,5 @@ let main = main "Unikernel.Main" ~packages
 let () = register "simple-fw" [ main
                                  $ public_netif    $ private_netif
                                  $ public_ethernet $ private_ethernet
-                                 $ public_arpv4    $ private_arpv4
-                                 $ public_ipv4     $ private_ipv4
                                  $ default_random  $ default_monotonic_clock
                                ]
